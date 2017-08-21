9c0474631ddfa6014029e6d56c117def7c67a4ff
himanjim <himanshu4529.pec@gmail.com>
2017-08-21 11:30:13 +0530

    Peer-graded Assignment
    Course 4 Project 1 Exploratory data analysis

---
+++ plot1.R
@@ -0,0 +1,17 @@
+downloadURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+downloadFile <- "household_power_consumption.zip"
+householdFile <- "household_power_consumption.txt"
+##
+if (!file.exists(householdFile)) {
+  download.file(downloadURL, downloadFile)
+  unzip(downloadFile)
+}
+##
+plotData <- read.table(householdFile, header=TRUE, sep=";", na.strings="?")
+## set time variable
+finalData <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
+SetTime <-strptime(paste(finalData$Date, finalData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+finalData <- cbind(SetTime, finalData)
+##
+## Generating Plot 1
+hist(finalData$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
